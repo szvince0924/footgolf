73a5696fa9d11c72fc6d44ad35315ad7ecaa6a1c
szvince0924 <163414915+szvince0924@users.noreply.github.com>
2024-12-10 12:44:23 +0100

    Add project files.

---
+++ footgolf/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace footgolf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzok> versenyzok = new();
+
+            foreach (var sor in File.ReadAllLines("fob2016.txt"))
+            {
+                versenyzok.Add(new Versenyzok(sor));  
+            }
+
+
+
+            Console.WriteLine($"3.feladat: versenyzok szama:{versenyzok.Count} ");
+
+            int versenyzok_szama = versenyzok.Count;
+            int noi = 0;
+            foreach (var v in versenyzok)
+            {
+                if (v.Kategoria =="noi")
+                {
+                    noi++;
+                }
+            }
+            Console.WriteLine($"4.feladat: A Noi versenyzok aranya: ");
+        }
+     
+
+      
+    }
+}
---
+++ footgolf/Versenyzok.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace footgolf
+{
+    internal class Versenyzok
+    {
+        public string Nev { get; set; }
+
+        public string Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+        public int Fordulok { get; set; }
+
+
+        public Versenyzok(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            Nev = s[0];
+            Kategoria = s[1];
+            Egyesulet = s[2];
+            Fordulok = int.Parse(s[3]);
+
+        }
+
+
+
+
+
+    }
+
+ 
+}
